ad8873992bd1e28725992d7bf4ebfa0dd4d45eb2
Siddhant Amin <siddhant.amin@icloud.com>
2026-02-21 10:44:20 +0530

    Initial main.py created

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+# Calculator main file
+
+def main():
+    print("Calculator Application")
+    print("Features will be added from feature branches")
+
+if __name__ == "__main__":
+    main()
